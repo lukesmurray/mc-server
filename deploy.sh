0756917c4a1605ab64b8d87cad90eb6a1be10760
Luke Murray <lukepigeonmail@gmail.com>
2019-07-13 15:08:22 -0700

    fix script bug and path bug

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo "outputting info for lambda deploy"
 terraform output -json | jq 'with_entries(.value |= .value)' > mc-lambda/config.json
 
 echo "installing serverless plugings"
-./mc-lambda/install_serverless.sh
+./mc-lambda/scripts/install_serverless.sh
 
 echo "deploying lambda"
-./mc-lambda/deploy_serverless.sh
+./mc-lambda/scripts/deploy_serverless.sh
